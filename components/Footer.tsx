3c4fc60b3d0beb9137d19c34b01506925daec1e8
Raj Soren <7rajkumarsoren@gmail.com>
2025-07-05 13:59:21 +0530

    made  footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,61 +1,57 @@
 
 const Footer = () => {
   return (
-    <div className="bg-slate-950 pt-10 flex flex-col md:flex-row justify-between md:px-20 xl:px-44 p-16 gap-6">
-      <div className="flex flex-col gap-4">
-        <div className="text-white font-bold text-md">Products</div>
-        <ul className="text-gray-500/100 text-sm md:text-base space-y-4">
-          <li>Features</li>
-          <li>Integrations</li>
-          <li>Updates</li>
-          <li>FAQ</li>
-          <li>Pricing</li>
-        </ul>
+    <footer className="bg-slate-600 p-4 pt-8">
+      <div className="max-w-6xl mx-auto pt-4 text-slate-900">
+        <div className="flex md:justify-center justify-between flex-wrap">
+          <div className="space-y-2 w-fit mx-auto">
+            <h1 className="text-slate-950 md:text-lg text-base">Products</h1>
+            <ul className="space-y-2 text-gray-300/60">
+              <li>Features</li>
+              <li>Integrations</li>
+              <li>Updates</li>
+              <li>FAQ</li>
+              <li>Pricing</li>
+            </ul>
+          </div>
+          <div className="space-y-2 w-fit mx-auto">
+            <h1 className="text-slate-950 md:text-lg text-base">Company</h1>
+            <ul className="space-y-2 text-gray-300/60">
+              <li>About</li>
+              <li>Blog</li>
+              <li>Careers</li>
+              <li>Manifesto</li>
+              <li>Press</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="space-y-2 w-fit mx-auto">
+            <h1 className="text-slate-950 md:text-lg text-base">Resources</h1>
+            <ul className="space-y-2 text-gray-300/60">
+              <li>Examples</li>
+              <li>Community</li>
+              <li>Guides</li>
+              <li>Docs</li>
+            </ul>
+          </div>
+          <div className="space-y-2 w-fit mx-auto">
+            <h1 className="text-slate-950 md:text-lg text-base">Legal</h1>
+            <ul className="space-y-2 text-gray-300/60">
+              <li>Privacy</li>
+              <li>Terms</li>
+              <li>Security</li>
+            </ul>
+          </div>
+          <div className="space-y-2 md:w-auto w-full md:p-0 p-4">
+            <h1 className="text-slate-950 md:text-lg text-base">News-letter</h1>
+            <div className="overflow-hidden gap-1 flex flex-row  pr-2 pt-2">
+              <input type="email" placeholder="johndoe@gmail.com" className="rounded-md bg-gray-400 w-full px-2 py-1" />
+              <button className="border-0 text-gray-300 bg-neutral-800 outline-none rounded-xl px-3 py-1" type="submit">Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <div className="text-white font-bold text-md">Company</div>
-        <ul className="text-gray-500/100 text-sm md:text-base space-y-4">
-          <li>About</li>
-          <li>Blog</li>
-          <li>Careers</li>
-          <li>Manifesto</li>
-          <li>Press</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="text-white font-bold text-md">Resources</div>
-        <ul className="text-gray-500/100 text-sm md:text-base space-y-4">
-          <li>Examples</li>
-          <li>Community</li>
-          <li>Guides</li>
-          <li>Docs</li>
-        </ul>
-      </div>
-      <div className="flex flex-col gap-4">
-        <div className="text-white font-bold text-md">Legal</div>
-        <ul className="text-gray-500/100 text-sm md:text-base space-y-4">
-          <li>Privacy</li>
-          <li>Terms</li>
-          <li>Security</li>
-        </ul>
-      </div>
-      <div className="space-y-6 col-span-2">
-        <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100">
-          News-letter
-        </h1>
-        <form className="w-full max-w-2xl flex flex-col sm:flex-row gap-3">
-          <input
-            type="email"
-            placeholder="johndoe@gmail.com"
-            className="px-5 py-2.5 rounded-md outline-none flex-1 bg-gray-200 dark:bg-gray-800"
-          />
-          <button className="outline-none w-full py-2.5 px-5 sm:w-max bg-blue-600 text-white rounded-md flex items-center justify-center">
-            Subscribe
-          </button>
-        </form>
-      </div>
-    </div>
+    </footer>
   );
 };
 
